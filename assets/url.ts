e833554a962f6a5136c4a2964112d5dbf3d1ee7a
abhay420coder <ak8294836065@gmail.com>
2023-10-01 21:50:19 +0530

    Auto-generated commit [ci skip]

--- assets/url.ts
+++ assets/url.ts
@@ -1,4 +1,6 @@
+import { env } from "src/app/env/env.dev"
 export const globalUrls = {
-    TODO_LOCOL_URL : 'http://localhost:5000/api/todoLists/',
-    TODO_RENDER_URL : 'https://backend-assignment-sdet.onrender.com/api/todoLists/'
+    TODO_URL : env.BASE_URL +'api/todoLists/',
+    // TODO_LOCOL_URL : 'http://localhost:5000/api/todoLists/',
+    // TODO_RENDER_URL : 'https://backend-assignment-sdet.onrender.com/api/todoLists/'
 }
