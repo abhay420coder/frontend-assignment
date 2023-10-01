ed557d2242cd9cc45751e7b8758cbecf24fc306c
abhay420coder <ak8294836065@gmail.com>
2023-10-01 21:47:25 +0530

    -update env

---
+++ src/app/env/env.prod.ts
@@ -0,0 +1,22 @@
+export const env = {
+    production:true,
+    BASE_URL:'https://backend-assignment-sdet.onrender.com/',
+}
+
+
+/*
+export const env = {
+    production:false,
+    BASE_URL :'http://localhost:5000/',
+    CUSTOMER_:{
+        GET_ALL_CUSTOMERS:'getallcustomer'
+        GET_CUSTOMERS:'getonecustomer',
+        UPDATE_CUSTOMERS:'updatecustomer'
+        ADD_CUSTOMERS:'addcustomer'
+    },
+    LOANS:{
+        .....
+    },
+    ......
+}
+*/
